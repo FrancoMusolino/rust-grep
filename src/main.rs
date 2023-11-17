6dfe84a2c024bba73fa66b6003decf0f4b420c7e
FrancoMusolino <francomusolino55@gmail.com>
2023-11-17 12:27:55 -0300

    read args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::env;
+use std::fs;
+use std::process;
+
+struct Commands {
+    file_path: String,
+    query: String,
+}
+
+impl Commands {
+    fn new(args: &[String]) -> Result<Self, String> {
+        if args.len() != 2 {
+            return Err(format!("Expected 2 args, found {}", args.len()));
+        }
+
+        Ok(Self {
+            file_path: args[0].clone(),
+            query: args[1].clone(),
+        })
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let env_args: Vec<String> = env::args().collect();
+    let args = &env_args[1..];
+
+    let commands: Commands = Commands::new(args).unwrap_or_else(|err| {
+        println!("Error parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    let contents =
+        fs::read_to_string(commands.file_path).expect("Should have been able to read the file");
+
+    println!("{contents}");
 }
