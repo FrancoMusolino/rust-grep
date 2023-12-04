c4830558bb670d3c6ffbf42a57e01bad4418896b
FrancoMusolino <francomusolino55@gmail.com>
2023-12-04 15:57:41 -0300

    search fn and verbose flag

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,22 @@
+use colored::Colorize;
 use std::error::Error;
 use std::fs;
 pub struct Config {
-    pub file_path: String,
-    pub query: String,
+    file_path: String,
+    query: String,
+    flags: Vec<String>,
 }
 
 impl Config {
     pub fn build(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() != 2 {
-            return Err("Expected 2 args");
+        if args.len() < 2 {
+            return Err("Expected at least 2 args");
         }
 
         Ok(Self {
             file_path: args[0].clone(),
             query: args[1].clone(),
+            flags: args[2..].to_vec(),
         })
     }
 }
@@ -21,7 +24,48 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    let matches = search(&config.query, &contents);
+
+    for m in matches.iter() {
+        println!("{m}")
+    }
+
+    if config.flags.contains(&String::from("--verbose")) {
+        println!();
+        println!("{}", "Verbose:".bold());
+        println!();
+
+        for line in contents.lines() {
+            if matches.contains(&line) {
+                println!("{}", line.green())
+            } else {
+                println!("{line}")
+            }
+        }
+    }
 
     Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
